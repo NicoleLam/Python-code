77250cbd42d7dec8912e3706a577028e14820b2d
Nicole Lam <nicolefranceslam@gmail.com>
2019-05-16 23:30:33 -0400

    code for problem 'plus one'

---
+++ plus-one.py
@@ -0,0 +1,11 @@
+'''
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+'''
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = int(''.join(str(i) for i in digits)) +1
+        return [int(x) for x in str(num)]
